112ca5079bb8320c77e6e8419058a2ea9b71fb57
letgivi <94454048+letgivi@users.noreply.github.com>
2026-02-23 20:59:03 +0700

    Delete theme directory

--- theme/assets/js/dfp.min.js
+++
@@ -1,7 +0,0 @@
-/* 
- * Used for adblocker detection
- */
-var e=document.createElement('div');
-e.id='iHCIjECYmrkE';
-e.style.display='none';
-document.body.appendChild(e);
--- theme/assets/offline/_READ_ME_FOR_FONTS.txt
+++
@@ -1,3 +0,0 @@
-Fonts for the logo can be downloaded here:
-
-https://fonts.google.com/specimen/IBM+Plex+Sans
